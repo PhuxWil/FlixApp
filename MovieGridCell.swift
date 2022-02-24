1527b9b6f322fad8c607e63f27b8dba50eb8c6c2
PhuxWil <97001771+PhuxWil@users.noreply.github.com>
2022-02-24 16:12:05 -0500

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix App
+//
+//  Created by Wilson Phu on 2/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
